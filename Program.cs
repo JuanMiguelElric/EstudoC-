3bec04c0bb193095ed3818947f36cbe8fa12fb69
Juan Miguel <oliveirajuan243@gmail.com>
2023-12-05 00:58:29 -0300

    MAis umas aula da Dio

--- Program.cs
+++ Program.cs
@@ -9,3 +9,34 @@
 // pessoa1.
 // int a =Convert.ToInt32("5");
 // Console.WriteLine(a);
+
+// calculadora
+// using LearningC_.models;
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(6,8);
+// calc.Multiplicar(2,6);
+// calc.Dividir(8,2);
+// calc.Potencia(3,3);
+
+
+
+
+int[] arrayInteiros = new int[4];
+arrayInteiros[0] = 72;
+arrayInteiros[1]=64;
+arrayInteiros[2]=50;
+arrayInteiros[3]=1;
+
+for(int i=0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posicao{i}-{arrayInteiros[i]}");
+
+}
+// percorrendo array com FOREACH
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine(valor);
+}
+
--- models/Calculadora.cs
+++ models/Calculadora.cs
@@ -13,9 +13,27 @@ namespace LearningC_.models
         }
         public void Subtrair(int x, int y)
         {
-            Console.WriteLine($"{x} - {y}= {x-y}")
+            Console.WriteLine($"{x} - {y}= {x-y}");
+
         }
         public void Multiplicar(int x, int y)
-
+        {
+            Console.WriteLine($"{x} x {y} = {x * y}");
+        }
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x}:{y}= {x/y}");
+        }
+        public void Potencia(int x, int y)
+        {   
+            double pot =Math.Pow(x,y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI/100;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Sendo de {angulo}={seno}º");
+        }
     }
 }
